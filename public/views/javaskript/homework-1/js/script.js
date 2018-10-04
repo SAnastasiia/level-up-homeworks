fa82524ace13156c6c5addf972e0bb678fb907a9
SAnastasiia <sysoevaanastasia42@gmail.com>
2018-10-04 16:06:12 +0300

    add folder js

---
+++ public/views/javaskript/homework-1/js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+var admin, name;
+name = 'Vasiliy';
+admin = name;
+console.log(admin);
+
+var ourPlanet = 'Earth';
+var userName = 'Petya';
+
+var fps; //frames per second
+var signBetweenWords;
+var documentView;
