ebf76c17b5c41eeeedad8c83d6217210c85cfc72
SAnastasiia <sysoevaanastasia42@gmail.com>
2018-10-20 18:38:21 +0200

    fixed 2 HW

--- public/views/javasсript/homework-2/js/script.js
+++ public/views/javasсript/homework-2/js/script.js
@@ -15,14 +15,14 @@ console.log(b); // 3
 
 
 
-//Чему будет равен x в примере ниже?
+// Чему будет равен x в примере ниже?
 
 var a = 2;
 
 var x = 1 + (a *= 2);
 
 
-//x = 1 + (a=2*2)= 1+4=5
+// x = 1 + (a=2*2)= 1+4=5
 x=5;
 console.log(x);
 
@@ -34,8 +34,8 @@ var b = 4;
 var c = (b += 5) - a++ * b - 3 + (b -= 3 + 1 - --a);
 console.log(c);
 console.log(b);
-//1) b=b+5  b=9 we've used combined addition operator
-//2) a++ -> the postfix form first returns old values and we need to keep in mind new values
-//3) a*b =2*9=18 multiply operator with two variables
-//4) b = 9-(3+1-2); First we've used combined subtraction operator and than used the prefix form of decrement operator for "a" and made an arithmetic(plus and minus) operations
-//5) 9-18-3+7=-5
+// 1) b=b+5  b=9 we've used combined addition operator
+// 2) a++ -> the postfix form first returns old values and we need to keep in mind new values
+// 3) a*b =2*9=18 multiply operator with two variables
+// 4) b = 9-(3+1-2); First we've used combined subtraction operator and than used the prefix form of decrement operator for "a" and made an arithmetic(plus and minus) operations
+// 5) 9-18-3+7=-5
